9418195cf8c3f32eeedd7eb39b5a345011ae3eb5
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-03-17 13:59:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function () {
+    var o = {
+        posts: [
+            { title: 'post 1', upvotes: 5 },
+            { title: 'post 2', upvotes: 2 },
+            { title: 'post 3', upvotes: 15 },
+            { title: 'post 4', upvotes: 9 },
+            { title: 'post 5', upvotes: 4 }
+        ]
+    };
+    return o;
+}]);
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function ($scope, posts) {
+        $scope.posts = posts.posts;
+
+        $scope.addPost = function () {
+            if (!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
+            $scope.comments = '';
+        };
+
+        $scope.incrementUpvotes = function (post) {
+            post.upvotes += 1;
+        };
+
+    }]);
